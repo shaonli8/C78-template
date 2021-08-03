a830d22b592b188a495c2c8e6f81d1aecd08de51
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-08-03 14:02:37 +0530

    Create showModal.js

---
+++ showModal.js
@@ -0,0 +1,106 @@
+showModal = ()=>{
+  return(
+  <Modal
+    animationType="fade"
+    transparent={true}
+    visible={this.state.isModalVisible}
+    >
+    <View style={styles.modalContainer}>
+      <ScrollView style={{width:'100%'}}>
+        <KeyboardAvoidingView style={styles.KeyboardAvoidingView}>
+        <Text
+          style={styles.modalTitle}
+          >Registration</Text>
+        <TextInput
+          style={styles.formTextInput}
+          placeholder ={"First Name"}
+          maxLength ={8}
+          onChangeText={(text)=>{
+            this.setState({
+              firstName: text
+            })
+          }}
+        />
+        <TextInput
+          style={styles.formTextInput}
+          placeholder ={"Last Name"}
+          maxLength ={8}
+          onChangeText={(text)=>{
+            this.setState({
+              lastName: text
+            })
+          }}
+        />
+        <TextInput
+          style={styles.formTextInput}
+          placeholder ={"Contact"}
+          maxLength ={10}
+          keyboardType={'numeric'}
+          onChangeText={(text)=>{
+            this.setState({
+              contact: text
+            })
+          }}
+        />
+        <TextInput
+          style={styles.formTextInput}
+          placeholder ={"Address"}
+          multiline = {true}
+          onChangeText={(text)=>{
+            this.setState({
+              address: text
+            })
+          }}
+        />
+        <TextInput
+          style={styles.formTextInput}
+          placeholder ={"Email"}
+          keyboardType ={'email-address'}
+          onChangeText={(text)=>{
+            this.setState({
+              emailId: text
+            })
+          }}
+        /><TextInput
+          style={styles.formTextInput}
+          placeholder ={"Password"}
+          secureTextEntry = {true}
+          onChangeText={(text)=>{
+            this.setState({
+              password: text
+            })
+          }}
+        /><TextInput
+          style={styles.formTextInput}
+          placeholder ={"Confrim Password"}
+          secureTextEntry = {true}
+          onChangeText={(text)=>{
+            this.setState({
+              confirmPassword: text
+            })
+          }}
+        />
+        <View style={styles.modalBackButton}>
+          <TouchableOpacity
+            style={styles.registerButton}
+            onPress={()=>
+              this.userSignUp(this.state.emailId, this.state.password, this.state.confirmPassword)
+            }
+          >
+          <Text style={styles.registerButtonText}>Register</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.modalBackButton}>
+          <TouchableOpacity
+            style={styles.cancelButton}
+            onPress={()=>this.setState({"isModalVisible":false})}
+          >
+          <Text style={{color:'#ff5722'}}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </View>
+  </Modal>
+)
+}
